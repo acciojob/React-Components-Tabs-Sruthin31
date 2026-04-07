9d397a8a9ef4ebc676d667fe82801b4e3bc53693
Sruthin31 <153161309+Sruthin31@users.noreply.github.com>
2026-04-07 07:55:47 +0000

    run

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -5,7 +5,7 @@ function Tabs(){
                 <div className="tab" id="Home-tab">Home</div>
                 <div className="tab" id="About-tab">About</div>
                 <div className="tab" id="Features-tab">Features</div>
-                <div className="viewport">Pages Go Here.</div>
+                <div className="viewport">Pages Go Here</div>
             </div>
     )
 
