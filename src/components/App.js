cc840ed1464346456df79670262f499d2a6fc546
Sruthin31 <153161309+Sruthin31@users.noreply.github.com>
2026-04-07 07:44:34 +0000

    running

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ function App(){
                 <div className="tab" id="Home-tab">Home</div>
                 <div className="tab" id="About-tab">About</div>
                 <div className="tab" id="Features-tab">Features</div>
-                <div className="viewport">Pages Go Here</div>
+                <div className="viewport">Pages Go Here.</div>
             </div>
             </div>
     )
