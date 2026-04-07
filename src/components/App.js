6de107137c691bcd7b1c9403ef056f15ae9997c4
Sruthin31 <153161309+Sruthin31@users.noreply.github.com>
2026-04-07 07:42:19 +0000

    runcode

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,15 @@
+import React from "react";
+function App(){
+    return(
+        <div>
+            <div className="tabs">
+                <div className="tab" id="Home-tab">Home</div>
+                <div className="tab" id="About-tab">About</div>
+                <div className="tab" id="Features-tab">Features</div>
+                <div className="viewport">Pages Go Here</div>
+            </div>
+            </div>
+    )
 
+}
+export default App;
