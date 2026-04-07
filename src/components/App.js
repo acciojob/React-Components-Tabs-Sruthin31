9b42c7cae0f8a167fe983ea02b0baaae2f1ac66a
Sruthin31 <153161309+Sruthin31@users.noreply.github.com>
2026-04-07 07:46:26 +0000

    coderun

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 function App(){
     return(
-        <div>
             <div className="tabs">
                 <div className="tab" id="Home-tab">Home</div>
                 <div className="tab" id="About-tab">About</div>
                 <div className="tab" id="Features-tab">Features</div>
                 <div className="viewport">Pages Go Here.</div>
-            </div>
             </div>
     )
 
